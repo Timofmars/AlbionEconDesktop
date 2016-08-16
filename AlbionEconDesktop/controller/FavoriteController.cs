dd87ef380e171679b525d6168263947bc49e899b
Andreas Kristoffersen <Krissam@gmail.com>
2016-08-16 14:35:56 +0200

    added list of favorite

--- AlbionEconDesktop/controller/FavoriteController.cs
+++ AlbionEconDesktop/controller/FavoriteController.cs
@@ -1,20 +1,33 @@
 ﻿using AlbionEconDesktop.model;
 using AlbionEconDesktop.storage;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlbionEconDesktop.controller
 {
     class FavoriteController
     {
+        private static List<Item> _favorites;
+        public static List<Item> List
+        {
+            get
+            {
+                if (_favorites == null) _favorites = new List<Item>(Item.All.Where(i => i.IsFavorite));
+                return _favorites;
+            }
+        }
         public static void AddFavorite(Item item)
         {
             item.IsFavorite = true;
             FavoriteStorage.Add(item);
+            List.Add(item);
         }
         public static void RemoveFavorite(Item item)
         {
             item.IsFavorite = false;
             FavoriteStorage.Remove(item);
+            List.Remove(item);
         }
     }
 }
